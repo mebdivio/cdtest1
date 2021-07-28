682aafbbc1f522fb2f16449a423dddfbf87fdf1d
Mebrahtu <mebrahtu.tesfazghi@divio.ch>
2021-07-28 17:49:01 +0200

    Initial commit to test deployment status

---
+++ .github/workflows/cd-test.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+while true; do 
+  sleep 1
+  DEPLOY="$(curl https://api.divio.com/apps/v3/deployments/$TEST_DEPLOYMENT_ID/ -H "Authorization: Token $API_TOKEN")"
+  echo "${DEPLOY}" | jq '.' > deploy.json
+  status="$(jq '.status' deploy.json)"
+  echo "${status} on progress"
+  success="$(jq '.success' deploy.json)"
+  echo "Success is ${success}"
+  if [ $success=='true' ] || [ $success=='false' ]; then
+    echo "Success inside if is $success"
+    if [ $success == 'true' ]; then 
+      echo "Success inside second if for true is $success"
+      echo "Deployment has completed successfully"
+      exit 0
+    else
+      echo "Success inside second if 'else' expecting to be false is $success"
+      echo "Deplyment has failed"
+      exit 1
+    fi
+    break
+  fi
+done
+ 
