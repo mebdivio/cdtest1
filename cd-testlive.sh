c9d0a5983d4a64a1a10ba8cec8039f2b0edb5ce6
Mebrahtu <mebrahtu.tesfazghi@divio.ch>
2021-11-19 11:32:38 +0100

    Testing the live deployment

--- cd-testlive.sh
+++ cd-testlive.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+LIVE_DEPLOYMENT_ID="$(curl -X POST --data "environment=$LIVE_ENVIRONMENT_ID" --header "Authorization: Token $API_TOKEN" https://api.divio.com/apps/v3/deployments/ | jq '.uuid')"
+echo "The deployment id is: $LIVE_DEPLOYMENT_ID"
+
 while true; do 
   sleep 1
   DEPLOY="$(curl https://api.divio.com/apps/v3/deployments/$LIVE_DEPLOYMENT_ID/ -H "Authorization: Token $API_TOKEN")"
@@ -8,18 +11,16 @@ while true; do
   echo "${status} on progress"
   success="$(jq '.success' deploy.json)"
   echo "Success is ${success}"
-  if [ $success=='true' ] || [ $success=='false' ]; then
-    echo "Success inside if is $success"
-    if [ $success == 'true' ]; then 
-      echo "Success inside second if for true is $success"
+  if [ $success == 'true' ]; then 
+      echo "Success for true is $success"
       echo "Deployment has completed successfully"
       exit 0
-    else
-      echo "Success inside second if 'else' expecting to be false is $success"
+      break
+  elif [ $success == 'fals' ]; then 
+      echo "Success for false is $success"
       echo "Deplyment has failed"
       exit 1
-    fi
-    break
+      break
   fi
 done
  
