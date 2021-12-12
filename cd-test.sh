239d20e230eb6e816f9593761d10136a380579ba
Mebrahtu <mebrahtu.tesfazghi@divio.ch>
2021-12-12 15:13:28 +0100

    Updated develop branch for local testing

--- cd-test.sh
+++ cd-test.sh
@@ -1,33 +1,39 @@
-# #! /bin/bash
-# echo "The Project slug is: ${PROJECT_SLUG}"
-# export APPLICATION_ID=$(curl https://api.divio.com/apps/v3/applications/\?slug\=$PROJECT_SLUG -H "Authorization: Token $API_TOKEN" | jq '.results.uuid'| tr -d '"')
+#! /bin/bash
 
-# echo "The APPLICATION_ID is: ${PROJECT_SLUG}"
-# export TEST_ENVIRONMENT_ID=$(curl -X POST --data "environment=$TEST_ENVIRONMENT_ID" --header "Authorization: Token $API_TOKEN" https://api.divio.com/apps/v3/deployments/ | jq '.result.uuid'| tr -d '"')
+# From the Project SLUG, json Querying the API to get the APPLICATION UUID, removing the quotation mark
+APPLICATION_UUID=$(curl https://api.divio.com/apps/v3/applications/\?slug\=$PROJECT_SLUG -H "Authorization: Token $API_TOKEN" | jq '.results[0].uuid'| tr -d '"')
 
+# From the APPLICATION UUID, getting the ENVIRONMENT UUID
+TEST_ENVIRONMENT_UUID=$(curl https://api.divio.com/apps/v3/environments/\?application\=$APPLICATION_UUID -H "Authorization: Token $API_TOKEN" | jq '.results[0].uuid'| tr -d '"')
 
-export TEST_DEPLOYMENT_ID=$(curl -X POST --data "environment=$TEST_ENVIRONMENT_ID" --header "Authorization: Token $API_TOKEN" https://api.divio.com/apps/v3/deployments/ | jq '.uuid'| tr -d '"')
-echo "The deployment is: ${TEST_DEPLOYMENT_ID}"
+# From the ENVIRONMENT UUID, getting the DEPLOYMENT UUID
+TEST_DEPLOYMENT_UUID=$(curl -X POST --data "environment=$TEST_ENVIRONMENT_UUID" --header "Authorization: Token $API_TOKEN" https://api.divio.com/apps/v3/deployments/ | jq '.uuid'| tr -d '"')
 
-while true; do 
+# Loop until deployment is completed
+while true; do
   sleep 1
-  echo "The deployment id is: ${TEST_DEPLOYMENT_ID}"
-  export DEPLOY=$(curl https://api.divio.com/apps/v3/deployments/$TEST_DEPLOYMENT_ID/ -H "Authorization: Token $API_TOKEN")
-  echo "${DEPLOY}" | jq '.' > deploy.json
+
+# From the DEPLOYMENT UUID, querying and echoing the DEPLOYMENT credentials to a json file
+  echo "$(curl https://api.divio.com/apps/v3/deployments/$TEST_DEPLOYMENT_UUID/ -H "Authorization: Token $API_TOKEN")" | jq '.' > deploy.json
+
+# Querying the current status of the deployment from the json file and broadcasting it
   STATUS="$(jq '.status' deploy.json)"
-  echo "${STATUS} in progress"
+  echo "Deployment ${STATUS}"
+
+# Querying the current success status of the deployment from the json file
   SUCCESS="$(jq '.success' deploy.json)"
-  echo "Success is ${SUCCESS}."
-  if [ $SUCCESS == true ]; then 
+
+# Checking the current success status of the deployment 
+# If success or failure report and exit, if not continue
+  if [ $SUCCESS == true ]; then
     echo "Deployment has completed successfully"
     exit 0
     break
-  elif [ $SUCCESS == false ]; then  
-    echo "Deplyment has failed"
+  elif [ $SUCCESS == false ]; then
+    echo "Deployment has failed"
     exit 1
     break
   else
-    echo "Deplyment has not yet finished"
+    continue
   fi
 done
- 
